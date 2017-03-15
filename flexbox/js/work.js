8179c005f6ed584d08107ade76667f0cc313c166
Ryan Achten <ryanachten@gmail.com>
2017-03-15 14:26:17 +1300

    Add partial carousel text functionality, split work js into .js file

---
+++ flexbox/js/work.js
@@ -0,0 +1,64 @@
+var projects = document.getElementsByClassName("project-cover");
+projectFadeIn(projects);
+rollover(projects);
+// carousel(projects);
+
+function projectFadeIn(projects){	
+	for (var i=0; i < projects.length; i++){
+		var curProjectId = '#' + projects[i].getAttribute('id');
+		$(curProjectId).delay(1000*i).fadeTo("slow", 1);
+	}
+}
+
+function rollover(coverImages){
+	for (var i = 0; i < coverImages.length; i++) {
+		coverImages[i].addEventListener("mouseenter", function(){
+			var coverRoll = this.children[0];
+			coverRoll.classList.toggle('rollover-active');
+		});
+		coverImages[i].addEventListener("mouseleave", function(){
+			var coverRoll = this.children[0];
+			coverRoll.classList.toggle('rollover-active');
+		});
+	}
+}
+
+function carousel(projects){
+	var bgUrls = [];
+	var rollovers = [];
+
+	for (var i=0; i < projects.length; i++){
+		var curProjectId = '#' + projects[i].getAttribute('id');
+		bgUrls.push( $(curProjectId).css('background-image'));
+		rollovers.push(projects[i].children[0]);
+	}
+
+	var projCounter = 0;
+	var header = $(".project-header");
+	var timer = setInterval(changeProject, 7000);
+
+	function changeProject(){
+		
+		header.fadeTo("slow", 0, function(){
+			changeText();
+			header.css('background-image', bgUrls[projCounter]);
+			header.fadeTo("slow", 1);
+		});
+		
+		if (projCounter == bgUrls.length-1){
+			console.log('reset projCounter');
+			return projCounter = 0;	
+		}else{
+			return projCounter++;
+		}	
+	}
+	function changeText(){
+		var projHeader = rollovers[projCounter].children[0].innerText;
+		console.log(projHeader);
+		// var headerDescript =;
+		var headerDescript = $(".project-header-descript");
+		headerDescript.children('h1').text(projHeader);
+
+
+	}
+}
