d850a5808c4f416dabeb81b52761ba70dd0f353c
Ryan Achten <ryanachten@gmail.com>
2017-03-17 14:35:22 +1300

    Add default accordian display hack

--- flexbox/js/cv_accord.js
+++ flexbox/js/cv_accord.js
@@ -3,6 +3,26 @@
  https://codepen.io/brenden/pen/Kwbpyj
 */
 
+$(document).ready(function(){
+    var width = $(window).width(); 
+    console.log('width: ' + width);
+    
+    if (width < 500){
+        console.log('close accordion');
+    }else{
+        // TODO: the function below is a hack. Fix properly
+        $('.toggle').each(function(){
+            var $this = $(this);
+            var curId = $this.next().attr('id');
+            if (curId !== "skills-container"){
+                console.log(curId);
+                 toggleAccordian($this);
+            }
+        });
+    }
+})
+
+// Click event
 $('.toggle').click(function(e) {
   	e.preventDefault();
   
@@ -12,11 +32,11 @@ $('.toggle').click(function(e) {
 
 function toggleAccordian(element){
     if (element.next().hasClass('show')) {
-        element.next().removeClass('show');
-        element.next().slideUp(350);
+            element.next().removeClass('show');
+            element.next().slideUp(350);
     } else {
-        // $this.parent().parent().find('li .inner').removeClass('show');
-        // $this.parent().parent().find('li .inner').slideUp(350);
+        // $this.parent().parent().find('.inner').removeClass('show');
+        // $this.parent().parent().find('.inner').slideUp(350);
         element.next().toggleClass('show');
         element.next().slideToggle(350);
     }
