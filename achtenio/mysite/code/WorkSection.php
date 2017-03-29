d358c2ac37b6e19f73d41dbcff076c5f9a4026be
Ryan Achten <ryanachten@gmail.com>
2017-03-29 14:08:49 +1300

    Add WorkSection dataobject for adding project content to project pages

---
+++ achtenio/mysite/code/WorkSection.php
@@ -0,0 +1,45 @@
+<?php
+	class WorkSection extends DataObject{
+
+		private static $db = array(
+			'Title' => 'Varchar',
+			'Description' => 'Text',
+		);
+
+		private static $has_one = array(
+			'Photo' => 'Image',
+			'WorkPage' => 'WorkPage'
+		);
+
+		private static $summary_fields = array(
+			'GridThumbnail' => '',
+			'Title' => 'Title',
+			'Description' => 'Description'
+		);
+
+		public function getGridThumbnail(){
+			if($this->Photo()->exists()){
+				return $this->Photo()->SetWidth(100);
+			}
+
+			return '(no image)';
+		}
+
+		public function getCMSFields(){
+			$fields = FieldList::create(
+				TextField::create('Title'),
+				TextareaField::create('Description'),
+				$uploader = UploadField::create('Photo')
+			);
+
+			$uploader->setFolderName('project-content/project-photos');
+			$uploader->getValidator()->setAllowedExtensions(array(
+				'png','gif','jpeg','jpg'));
+
+			return $fields;
+		}
+
+	}
+
+
+?>
